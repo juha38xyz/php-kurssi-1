95e8d94c60300c8900cd804bb0d91560f396682e
juha38xyz <61047216+juha38xyz@users.noreply.github.com>
2020-09-28 14:20:39 +0300

    Update index.php
    tehtävät 1-1 - 1-4

--- 1-2/index.php
+++ 1-2/index.php
@@ -1,1 +1,5 @@
 <?php
+$letters = "Hello";
+$number = 589;
+$decimal = 50.95;
+$table = ["a", "b", "c"];
--- 1-3/index.php
+++ 1-3/index.php
@@ -2,3 +2,4 @@
 $name = 'Mira';
 $age  = 28;
 
+echo "Nimeni On ".  $name. " ja olen ".$age." vuotta.";
