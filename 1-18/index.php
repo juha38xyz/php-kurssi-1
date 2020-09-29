3c1cad06404356aafac920c3e8f5082ee301d604
juha38xyz <61047216+juha38xyz@users.noreply.github.com>
2020-09-29 14:05:46 +0300

    Update index.php

--- 1-18/index.php
+++ 1-18/index.php
@@ -1,7 +1,33 @@
 <?php
 
-function generateNames()
+
+
+function generateNames(int $count, string $prefix = ""):array
 {
     $firstNames = ['Mikko', 'Matti', 'Antti', 'Simo', 'Anna', 'Siiri', 'Mira', 'Jaana'];
     $lastNames  = ['Meikäläinen', 'Onnekas', 'Mallikas', 'Testaaja', 'Esimerkkinen'];
+
+   if($count < 1 || $count > 20){
+       $count = 1;
+   }
+
+   if(strlen($prefix) > 20){
+       $prefix = "";
+   }
+
+   $result = [];
+
+   for($i = 0: $s < $count; $i++){
+       $first = $firstNames[rand(0, (count($firstNames )-1))];
+       $last = $lastNames[rand(0, (count($lastNames)-1 ))];
+
+       $result[] = $prefix . " " . $first . " " . $last;
+
+   }
+
+   return $result;
+
+
 }
+print_r();
+
