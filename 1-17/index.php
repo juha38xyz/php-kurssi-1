3d2ee7669591d9cfaedc20a11b6fbd43ade74f94
juha38xyz <61047216+juha38xyz@users.noreply.github.com>
2020-09-29 12:58:11 +0300

    Update index.php

--- 1-17/index.php
+++ 1-17/index.php
@@ -1,18 +1,33 @@
 <?php
 
-function first_function($word): string
+function first_function(string $word): string
 {
     return $word . " World!";
 }
+echo first_function("");
 
-function second()
+echo "<br>";
+
+function second(): float
 {
     return 123.30;
 }
+echo second();
 
-function third()
+echo "<br>";
+
+function third(string $letter): string 
 {
-    return ['A', 'B', $letter];
+    return $letter . 'A'. 'B';
 }
+echo third('');
+
+echo"<br>";
 
 // Tähän tulee fourth()
+function fourth(int $x, int $y)
+{
+    return $x + $y;
+}
+echo " Sum is: " . fourth(10, 2);
+echo "<br>";
