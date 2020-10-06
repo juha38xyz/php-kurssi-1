0beefd60e57090e23fc86c4515f83f1cec3ec812
juha38xyz <61047216+juha38xyz@users.noreply.github.com>
2020-10-06 13:53:45 +0300

    Update index.php

--- 1-20/index.php
+++ 1-20/index.php
@@ -2,4 +2,26 @@
 
 $rows = file(__DIR__ . '/maakunnat.txt');
 
-print_r($rows);
+$list =[];
+
+foreach($rows as $row){
+    $parts = explode(';' , $row);
+    $list[$parts[0]] = (int) $parts[1];
+
+
+}
+
+arsort($list);
+
+$text = "";
+
+foreach($list as $maakunta => $vakiluku){
+    if($vakiluku % 2 !==0 ){
+        $text .= "$maakunta; $vakiluku\n";
+    }
+
+}
+
+file_put_contents(
+    __DIR__ . '/maakunnat_vastaus.txt', $text);
+
